998640d70ed3ab694f71cec3854f5765abd44f32
Alexander Urizar <urizaraj@gmail.com>
2018-03-20 17:50:02 -0400

    chnges

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,14 @@
 class LineItemsController < ApplicationController
+  def create
+    if current_user.current_cart
+      current_user.current_cart.add_item(item_id)
+    else
+      cart = current_user.carts.build
+      li = cart.add_item(params[:item_id])
+      li.save
+      current_user.update(current_cart: cart)
+    end
 
+    redirect_to store_path
+  end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,6 @@
 class StoreController < ApplicationController
   def index
     @categories = Category.all
-    @items = Item.all
+    @items = Item.available_items
   end
 end
